30c8bf9910c100876845eb649717d2a445c99d48
nishank95 <nishanksamant@gmail.com>
2021-03-21 10:54:41 +1100

    commenting out SignupActivity as its not used at the moment

--- CupLogin-master/app/src/main/java/com/example/cuplogin/SignupActivity.java
+++ CupLogin-master/app/src/main/java/com/example/cuplogin/SignupActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Objects;
 
+// Not used at the moment
 public class SignupActivity extends AppCompatActivity {
 
     EditText emailET,passwordET,usernameET,userTypeET;
